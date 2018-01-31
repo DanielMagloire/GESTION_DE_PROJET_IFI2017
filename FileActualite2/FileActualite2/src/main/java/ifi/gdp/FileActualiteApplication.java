1e9a4127c10baaa3ed9f6a1cba648c220a7870bb
Daniel Magloire <31102788+DanielMagloire@users.noreply.github.com>
2018-02-01 04:09:11 +0700

    Add files via upload

---
+++ FileActualite2/FileActualite2/src/main/java/ifi/gdp/FileActualiteApplication.java
@@ -0,0 +1,31 @@
+package ifi.gdp;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ifi.gdp.dao.ActualiteRepository;
+import ifi.gdp.entities.Actualite;
+
+@SpringBootApplication
+public class FileActualiteApplication implements CommandLineRunner {
+
+	@Autowired
+	private ActualiteRepository actualiteRepository; //Je déclare cette objet et je demande à Spring de faire une injection
+	public static void main(String[] args) {
+		SpringApplication.run(FileActualiteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		//Pour enregistrer plusieurs actualités, nous allons faire une boucle car nous aurons besoin de faire la pagination
+		for(int i=0; i < 5; i++){
+		//Créons un objet actualité
+		Actualite actualite = new Actualite("Recrutement en master recherche en informatique SIM IFI 2018", "Administration IFI", new Date(), "ifi"+i+".jpg", "L'IFI lance <strong>le recrutement</strong> pour l'accès au master recherche SIM pour le compte de l'année académique 2018-2019");
+		actualiteRepository.save(actualite);
+		}
+	}
+}
